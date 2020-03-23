12ce48bd104a48a79c86d143742ac67b28424900
Everson Silva <eversonsilva9799@gmail.com>
2020-03-23 08:01:14 -0300

    Adicionar novos atributos

--- backend/src/app/entities/Link.js
+++ backend/src/app/entities/Link.js
@@ -2,6 +2,8 @@ module.exports = (sequelize, DataTypes) => {
   const Link = sequelize.define('Link', {
     originalUrl: DataTypes.STRING,
     shortenedUrl: DataTypes.STRING,
+    saved: DataTypes.BOOLEAN,
+    views: DataTypes.INTEGER,
   });
 
   return Link;
