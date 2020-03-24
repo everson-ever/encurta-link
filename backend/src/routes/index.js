75e846f364eb2d8fc1e641b941377cd9ce220757
Everson Silva <eversonsilva9799@gmail.com>
2020-03-23 21:23:09 -0300

    Adicionar rota de exclusão de link

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,5 +6,6 @@ const routes = express.Router();
 routes.get('/links', LinkController.index);
 routes.get('/links/:short', LinkController.show);
 routes.post('/links', LinkController.store);
+routes.delete('/links/:id', LinkController.destroy);
 
 export default routes;
