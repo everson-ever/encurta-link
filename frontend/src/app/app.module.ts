755d1cc7e6aecdee6f02a883ddfad89248cd8279
Everson Silva <eversonsilva9799@gmail.com>
2020-03-25 21:28:00 -0300

    Organizar imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+
+import { AppComponent } from './app.component';
 import { AuthLayoutComponent } from './layout/components/auth-layout/auth-layout.component';
 import { ContentLayoutComponent } from './layout/components/content-layout/content-layout.component';
 import { DashboardLayoutComponent } from './layout/components/dashboard-layout/dashboard-layout.component';
-
 import { NavbarDashboardComponent } from './layout/components/navbar-dashboard/navbar-dashboard.component';
 import { NavbarComponent } from './layout/components/navbar/navbar.component';
 import { FooterComponent } from './layout/components/footer/footer.component';
