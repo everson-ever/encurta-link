4c04828877b8665233cc88df7e9abe254ca2cb07
Everson Silva <eversonsilva9799@gmail.com>
2020-03-28 09:26:00 -0300

    Model de usuário

---
+++ backend/src/app/entities/User.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+  });
+
+  User.associate = function(models) {
+    User.hasMany(models.Link, { foreignKey: 'userId' });
+  };
+
+  return User;
+};
