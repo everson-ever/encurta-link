b9c8cb6b203578a7f0a21a4b3947e4c24248f054
Everson Silva <eversonsilva9799@gmail.com>
2020-03-28 09:23:36 -0300

    Criar controller de usuário

---
+++ backend/src/app/controllers/UserController.js
@@ -0,0 +1,13 @@
+class UserController {
+  async index(req, res) {}
+
+  async show(req, res) {}
+
+  async store(req, res) {}
+
+  async update(req, res) {}
+
+  async destroy(req, res) {}
+}
+
+export default new UserController();
