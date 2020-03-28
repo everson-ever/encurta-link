4311dd602368a209c70b2201e39c05a2b8e011ea
Everson Silva <eversonsilva9799@gmail.com>
2020-03-28 09:25:16 -0300

    Adicionar rotas de usuários

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import LinkController from '../app/controllers/LinkController';
+import UserController from '../app/controllers/UserController';
 
 const routes = express.Router();
+
+routes.get('users', UserController.index);
+routes.get('users/:id', UserController.show);
+routes.post('users', UserController.store);
+routes.put('users/:id', UserController.update);
+routes.delete('users/:id', UserController.destroy);
 
 routes.get('/links', LinkController.index);
 routes.get('/links/:short', LinkController.show);
