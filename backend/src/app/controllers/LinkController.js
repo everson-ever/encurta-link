d71d796029f2bfffe876688798f1308730637037
Everson Silva <eversonsilva9799@gmail.com>
2020-03-28 18:04:51 -0300

    Organizar código

--- backend/src/app/controllers/LinkController.js
+++ backend/src/app/controllers/LinkController.js
@@ -3,79 +3,81 @@ const generateShort = require('./../utils/generateShort');
 import Response from './../models/Response';
 
 class LinkController {
-	async index(req, res) {
-		try {
-			const links = await Link.findAll({
-				where: {
-					saved: true
-				}
-			});
-			return res.status(200).json(new Response(200, 'sucesso', links));
-		} catch (err) {
-			return res.status(500).json(new Response(500, err.message, null));
-		}
-	}
+  async index(req, res) {
+    try {
+      const links = await Link.findAll({
+        where: {
+          saved: true,
+        },
+      });
+      return res.status(200).json(new Response(200, 'sucesso', links));
+    } catch (err) {
+      return res.status(500).json(new Response(500, err.message, null));
+    }
+  }
 
-	async show(req, res) {
-		try {
-			const { short } = req.params;
+  async show(req, res) {
+    try {
+      const { short } = req.params;
 
-			const link = await Link.findOne({
-				where: { shortenedUrl: short }
-			});
+      const link = await Link.findOne({
+        where: { shortenedUrl: short },
+      });
 
-			if (!link) {
-				res.status(404).json(new Response(404, 'recurso não encontrado', null));
-			}
+      if (!link) {
+        res.status(404).json(new Response(404, 'recurso não encontrado', null));
+      }
 
-			await link.views++;
-			await link.save();
+      await link.views++;
+      await link.save();
 
-			res.status(200).json(new Response(200, 'sucesso', { url: link.originalUrl }));
-		} catch (err) {
-			return res.status(500).json(new Response(500, err.message, null));
-		}
-	}
+      res
+        .status(200)
+        .json(new Response(200, 'sucesso', { url: link.originalUrl }));
+    } catch (err) {
+      return res.status(500).json(new Response(500, err.message, null));
+    }
+  }
 
-	async store(req, res) {
-		try {
-			const { originalUrl, saved } = req.body;
+  async store(req, res) {
+    try {
+      const { originalUrl, saved } = req.body;
 
-			const shortenedUrl = generateShort(5);
+      const shortenedUrl = generateShort(5);
 
-			const link = await Link.create({
-				originalUrl,
-				shortenedUrl,
-				saved: saved
-			});
+      const link = await Link.create({
+        originalUrl,
+        shortenedUrl,
+        saved: saved,
+      });
 
-			return res.status(201).json(new Response(201, 'sucesso', link));
-		} catch (err) {
-			return res.status(500).json(new Response(500, err.message, null));
-		}
-	}
+      return res.status(201).json(new Response(201, 'sucesso', link));
+    } catch (err) {
+      return res.status(500).json(new Response(500, err.message, null));
+    }
+  }
 
-	async destroy(req, res) {
-		try {
-			const { id } = req.params;
+  async destroy(req, res) {
+    try {
+      const { id } = req.params;
 
-			const link = await Link.findOne({
-				where: {
-					id: id
-				}
-			});
+      const link = await Link.findOne({
+        where: {
+          id: id,
+        },
+      });
 
-			if (!link) {
-				res.status(404).json(new Response(404, 'recurso não encontrado', null));
-			}
+      if (!link) {
+        res.status(404).json(new Response(404, 'recurso não encontrado', null));
+      }
 
-			await link.destroy();
+      await link.destroy();
 
-			res.status(201).json(new Response(201, 'sucesso', null));
-		} catch (err) {
-			return res.status(500).json(new Response(500, err.message, null));
-		}
-	}
+      res.status(201).json(new Response(201, 'sucesso', null));
+    } catch (err) {
+      return res.status(500).json(new Response(500, err.message, null));
+    }
+  }
 }
 
 export default new LinkController();
